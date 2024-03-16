ccab0d6adc78b03d014d66585b2e2e7ae657f70f
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2024-03-16 23:17:47 +0700

    update review

--- app/routes/reviewRouter.js
+++ app/routes/reviewRouter.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 
 // Route để tạo một đánh giá mới
-router.post('/reviews', reviewController.createReview);
+router.post('/', reviewController.createReview);
 
 // Route để lấy tất cả đánh giá
-router.get('/reviews', reviewController.getAllReviews);
+router.get('/', reviewController.getAllReviews);
 
 // Route để lấy một đánh giá theo ID
-router.get('/reviews/:id', reviewController.getReviewById);
+router.get('/:id', reviewController.getReviewById);
 
 // Route để cập nhật một đánh giá theo ID
-router.put('/reviews/:id', reviewController.updateReviewById);
+router.put('/:id', reviewController.updateReviewById);
 
 // Route để xóa một đánh giá theo ID
-router.delete('/reviews/:id', reviewController.deleteReviewById);
+router.delete('/:id', reviewController.deleteReviewById);
 
 module.exports = router;
