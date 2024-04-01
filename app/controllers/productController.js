fcd222a88d7a203bba049f9cec0f1548af0c9800
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2024-04-01 22:25:24 +0700

    update code

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -1,15 +1,22 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 
 // Controller để tạo một sản phẩm mới
 exports.createProduct = async (req, res) => {
   try {
-    const { name, price, description, category, percentSale, urlList } = req.body;
-    const newProduct = new Product({ name, price, description, category, percentSale, urlList });
+    const { name, price, description, category, priceSale, urlList } = req.body;
+    const newProduct = new Product({
+      name,
+      price,
+      description,
+      category,
+      priceSale,
+      urlList,
+    });
     const savedProduct = await newProduct.save();
     res.status(201).json(savedProduct);
   } catch (error) {
-    console.error('Error creating product:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error creating product:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -19,8 +26,8 @@ exports.getAllProducts = async (req, res) => {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
-    console.error('Error getting all products:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error getting all products:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -30,12 +37,12 @@ exports.getProductById = async (req, res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
     if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
     res.status(200).json(product);
   } catch (error) {
-    console.error('Error getting product by ID:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error getting product by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -43,19 +50,19 @@ exports.getProductById = async (req, res) => {
 exports.updateProductById = async (req, res) => {
   try {
     const productId = req.params.id;
-    const { name, price, description, category, percentSale } = req.body;
+    const { name, price, description, category, priceSale, urlList } = req.body;
     const updatedProduct = await Product.findByIdAndUpdate(
       productId,
-      { name, price, description, category },
+      { name, price, description, category, priceSale, urlList },
       { new: true }
     );
     if (!updatedProduct) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
     res.status(200).json(updatedProduct);
   } catch (error) {
-    console.error('Error updating product by ID:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error updating product by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -65,11 +72,11 @@ exports.deleteProductById = async (req, res) => {
     const productId = req.params.id;
     const deletedProduct = await Product.findByIdAndDelete(productId);
     if (!deletedProduct) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
     res.status(204).send();
   } catch (error) {
-    console.error('Error deleting product by ID:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error deleting product by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- app/models/productModel.js
+++ app/models/productModel.js
@@ -14,7 +14,7 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  percentSale:{
+  priceSale:{
     type: Number,
     required: true
   },
